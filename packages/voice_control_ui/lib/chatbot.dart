1b3a34789d4e61c6360f9ee81fe146db3d5b94c8
Alex Hartford <alex@oneblank.io>
2020-08-18 09:54:38 -0700

    Converted service to providers

--- packages/voice_control_ui/lib/chatbot.dart
+++ packages/voice_control_ui/lib/chatbot.dart
@@ -2,32 +2,46 @@ import 'package:dotenv/dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ibm_watson_assistant/ibm_watson_assistant.dart';
 
-class ChatbotService {
-  IbmWatsonAssistant bot;
-  String _sessionId;
-  String get sessionId => _sessionId;
+final botProvider = Provider<IbmWatsonAssistant>((ref) {
+  load();
 
-  ChatbotService() {
-    load();
+  final auth = IbmWatsonAssistantAuth(
+    assistantId: env['ASSISTANT_ID'],
+    url: env['ASSISTANT_URL'],
+    apikey: env['API_KEY'],
+  );
 
-    final auth = IbmWatsonAssistantAuth(
-      assistantId: env['ASSISTANT_ID'],
-      url: env['ASSISTANT_URL'],
-      apikey: env['API_KEY'],
-    );
+  return IbmWatsonAssistant(auth);
+});
 
-    this.bot = IbmWatsonAssistant(auth);
-  }
+final sessionIdProvider = StateProvider<String>((_) => null);
 
-  static final provider = Provider<ChatbotService>((_) => ChatbotService());
+// class ChatbotService {
+//   IbmWatsonAssistant bot;
+//   String _sessionId;
+//   String get sessionId => _sessionId;
 
-  Future<String> createSession() async {
-    _sessionId = await bot.createSession();
-    return _sessionId;
-  }
+//   ChatbotService() {
+//     load();
 
-  Future<void> deleteSession() async {
-    await bot.deleteSession(_sessionId);
-    _sessionId = null;
-  }
-}
+//     final auth = IbmWatsonAssistantAuth(
+//       assistantId: env['ASSISTANT_ID'],
+//       url: env['ASSISTANT_URL'],
+//       apikey: env['API_KEY'],
+//     );
+
+//     this.bot = IbmWatsonAssistant(auth);
+//   }
+
+//   static final provider = Provider<ChatbotService>((_) => ChatbotService());
+
+//   Future<String> createSession() async {
+//     _sessionId = await bot.createSession();
+//     return _sessionId;
+//   }
+
+//   Future<void> deleteSession() async {
+//     await bot.deleteSession(_sessionId);
+//     _sessionId = null;
+//   }
+// }
